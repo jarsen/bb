0553b944a2c35bd9ed93614e7efc43ed48ef041a
Jason Larsen <jlarsen@mavenlink.com>
2020-04-11 16:08:10 -0600

    initial pass at user registration model

---
+++ lib/bb/accounts/user.ex
@@ -0,0 +1,40 @@
+defmodule BB.Accounts.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :confirmed_at, :naive_datetime
+    field :email, :string
+    field :encrypted_password, :string
+
+    field :password, :string, virtual: true
+
+    timestamps()
+  end
+
+  @doc false
+  def registration_changeset(user, attrs) do
+    user
+    |> cast(attrs, [:email, :password])
+    |> validate_required([:email, :password])
+    |> validate_format(:email, "@")
+    |> validate_length(:email, max: 240)
+    |> validate_length(:password, min: 12, max: 80)
+    |> validate_format(:password, ~r/[a-z]/, message: "at least one lower case character")
+    |> validate_format(:password, ~r/[A-Z]/, message: "at least one upper case character")
+    |> validate_format(:password, ~r/[!?@#$%^&*_0-9]/,
+      message: "at least one digit or punctuation character"
+    )
+    |> maybe_encrypt_password()
+  end
+
+  defp maybe_encrypt_password(changeset) do
+    password = get_change(changeset, :password)
+
+    if password && changeset.valid? do
+      put_change(changeset, :encrypted_password, Bcrypt.hash_pwd_salt(password))
+    else
+      changeset
+    end
+  end
+end
---
+++ test/bb/accounts_test.exs
@@ -0,0 +1,48 @@
+defmodule BB.AccountsTest do
+  use BB.DataCase
+
+  alias BB.Accounts
+
+  describe "users" do
+    alias BB.Accounts.User
+
+    def fixture(:user, attrs \\ %{}) do
+      {:ok, user} =
+        attrs
+        |> Enum.into(%{
+          email: "bill@microsoft.com",
+          password: "some P4$sword"
+        })
+        |> Accounts.register_user()
+
+      user
+    end
+
+    test "get_user!/1 returns the user with given id" do
+      %{id: user_id} = fixture(:user)
+      assert %User{id: ^user_id} = Accounts.get_user!(user_id)
+    end
+
+    test "register_user/1 with valid data creates a user" do
+      assert {:ok, %User{} = user} =
+               Accounts.register_user(%{email: "bill@microsoft.com", password: "S3cureP4$$w0rD"})
+
+      assert user.email == "bill@microsoft.com"
+      assert user.encrypted_password != nil
+    end
+
+    test "register_user/1 with invalid email returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = Accounts.register_user(%{email: "not an email"})
+    end
+
+    test "register_user/1 with invalid password returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} =
+               Accounts.register_user(%{email: "bill@microsoft.com", password: "dolphins"})
+    end
+
+    test "registration_user/1 returns a user changeset" do
+      user = fixture(:user)
+      assert %Ecto.Changeset{} = Accounts.registration_user(user)
+    end
+  end
+end
